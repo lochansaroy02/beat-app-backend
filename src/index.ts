77029249432ddfa84694d9d67c6981e6cabce136
lochansaroy02 <lochansaroy47@gmail.com>
2025-10-03 22:26:29 +0530

    changed backend

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://dutytrack.vercel.app/en',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
